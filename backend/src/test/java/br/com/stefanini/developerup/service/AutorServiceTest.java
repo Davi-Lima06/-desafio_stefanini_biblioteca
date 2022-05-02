22a489abae442d591ac8c469e997cd67b20d8972
Davi Lima <davips22012@gmail.com>
2022-05-02 08:25:25 -0300

    mexendo nos testes

---
+++ backend/src/test/java/br/com/stefanini/developerup/service/AutorServiceTest.java
@@ -0,0 +1,27 @@
+package br.com.stefanini.developerup.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.wildfly.common.Assert;
+
+import br.com.stefanini.developerup.dto.AutorDto;
+
+class AutorServiceTest {
+
+	@Test
+	void test() throws Exception{
+		AutorService service = new AutorService();
+		AutorDto dto = new AutorDto();
+		dto.setBiografia("biografia");
+		dto.setDataNascimento("03/04/1999");
+		dto.setEmail("email#email");
+		dto.setISNI(158478158L);
+		dto.setNome("Davi");
+		
+		service.inserir(dto);
+		
+		Assert.assertTrue(dto.getBiografia().equals("biografia"));
+	}
+
+}
