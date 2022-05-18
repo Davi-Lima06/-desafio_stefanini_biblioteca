e9e227e832821e27deacd8aac0036fae7398b7fc
C1321240 Davi Lima E Silva <c1321240@interno.bb.com.br>
2022-05-18 17:09:05 -0300

    apagando lixos

--- frontend/src/app/views/emprestimos/listar-emprestimos/listar-emprestimos.component.ts
+++ frontend/src/app/views/emprestimos/listar-emprestimos/listar-emprestimos.component.ts
@@ -2,7 +2,7 @@ import { EmprestimosService } from './../emprestimos.service';
 import Emprestimo from 'src/app/global/models/emprestimo.model';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
-import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import ModeloEmprestimo from 'src/app/global/models/modeloEmprestimo.model';
 
 @Component({
@@ -18,7 +18,7 @@ export class ListarEmprestimosComponent implements OnInit {
   dataSource = new MatTableDataSource<ModeloEmprestimo>([]);
   tamanho: number = 0;
   displayedColumns = ['cliente', 'livro', 'data', 'acoes'];
-  constructor(private service: EmprestimosService, private render: Renderer2) {}
+  constructor(private service: EmprestimosService) {}
 
   ngOnInit(): void {
     this.service.listarEmprestimos().subscribe((emprestimos) => {
